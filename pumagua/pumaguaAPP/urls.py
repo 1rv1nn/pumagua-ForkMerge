69d3f5a395da468fee6107cfb1b5b0dcf9e3ec5e
1rv1nn <1rv1n@ciencias.unam.mx>
2024-04-13 19:31:10 -0600

    Initial Repository

---
+++ pumagua/pumaguaAPP/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+import pumaguaAPP.views as views
+
+app_name = "pumaguaAPP"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='Home')
+]
